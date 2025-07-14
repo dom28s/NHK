8fddff171f22eab8b9af71e539059265d841784a
Thanawat <stangthanawat2546@gmail.com>
2025-07-14 23:15:58 +0700

    layout re-design

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,19 +1,22 @@
 import { useNavigate } from "react-router-dom";
-import './Header.css'; 
+import './Header.css';
 
 export default function Header() {
   const navigate = useNavigate();
 
   return (
     <div className="header-container">
-      <div className="logo">
-        Token
+      <div className="header">
+        <div className="logo">
+          Token
+        </div>
+        <ul className="menu">
+          <li onClick={() => navigate("/favorite")}>FAVORITE</li>
+          <li onClick={() => navigate("/home")}>HOME</li>
+          <li onClick={() => navigate("/chat")}>CHAT</li>
+        </ul>
+
       </div>
-      <ul className="menu">
-        <li onClick={() => navigate("/favorite")}>FAVORITE</li>
-        <li onClick={() => navigate("/home")}>HOME</li>
-        <li onClick={() => navigate("/chat")}>CHAT</li>
-      </ul>
     </div>
   );
 }
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,24 +1,47 @@
 import { useEffect, useState } from "react";
 import "./home.css";
 import { useNavigate } from "react-router-dom";
+import Katakana from './../katakana/katakana';
+import Hiragana from './../hiragana/hiragana';
 export function Home() {
 
   const navigate = useNavigate()
   return (
     <div className="main-home">
-      <div className="content-card">
-        <div onClick={()=>navigate('/news')} className="news-card">
-          <h1>NEWS</h1>
-        </div>
-        <div className="kanna-card">
-          <div onClick={()=>navigate('/hira')} className="kata-hira-card">
-            <h3>hira</h3>
-          </div>
-          <div onClick={()=>navigate('/kata')} className="kata-hira-card">
-            <h3>ka</h3>
-          </div>
-        </div>
+  
+
+      <div className="banner-card">
+        <div onClick={()=>navigate('/chat')} className="chat-button"></div>
       </div>
+        <div className="content-box">
+          <div className="left-content"></div>
+          <div className="right-content">
+            <div onClick={()=>navigate('/news')} className="news-card "></div>
+            <div onClick={()=>navigate('/hira')} className="hira-card card-box">
+                <div className="hira-image kanna-image"></div>
+                <div className="hira-detail kanna-detail">Hiragana</div>
+            </div>
+            <div onClick={()=>navigate('/kata')} className="kata-card card-box">
+                <div  className="kata-image kanna-image"></div>
+                <div className="kata-detail kanna-detail">Katakana</div>
+
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
+
+      // <div className="content-card">
+      //   <div onClick={()=>navigate('/news')} className="news-card">
+      //     <h1>NEWS</h1>
+      //   </div>
+      //   <div className="kanna-card">
+      //     <div onClick={()=>navigate('/hira')} className="kata-hira-card">
+      //       <h3>hira</h3>
+      //     </div>
+      //     <div onClick={()=>navigate('/kata')} className="kata-hira-card">
+      //       <h3>ka</h3>
+      //     </div>
+      //   </div>
+      // </div>
